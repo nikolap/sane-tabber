001a42137af99973ced953a03800a3d6d4c64774
nikolap <nikola.peric03@gmail.com>
2016-03-12 11:25:48 -0500

    Critical points bug fix

--- src-cljs/sane_tabber/controllers/ballots.cljs
+++ src-cljs/sane_tabber/controllers/ballots.cljs
@@ -19,7 +19,7 @@
   (apply + (vals (get active-scores team-id))))
 
 (defn get-team-points [active-scores team-id]
-  (index-of (map first (sort-by #(apply + (second %)) > active-scores)) team-id))
+  (index-of (map first (sort-by #(apply + (second %)) < active-scores)) team-id))
 
 (defn send-ballot! [round-room ballot-data]
   (ws/send-transit-msg! (assoc round-room :ballot ballot-data) :round-rooms)
